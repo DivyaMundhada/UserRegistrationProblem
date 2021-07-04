ac3f4de9ffe8607bf35061aa8408278b399b78b1
DivyaMundhada <divyamundada9@gmail.com>
2021-07-04 22:36:37 +0530

    Added Welcome Message.

---
+++ src/main/java/com/bridgelabz/UserRegistrationUsingRegex/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.UserRegistrationUsingRegex;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to User Registration Program.");
+
+	}
+
+}
